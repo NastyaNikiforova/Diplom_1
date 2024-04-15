f572970475477390dc48c282388d1189ab0af875
NastyaNikiforova <danilova-anastaska@yandex.ru>
2024-04-15 12:33:05 +0300

    в BurgerTest.java добавлен тест метода moveIngredients обновлен отчет Jacoco

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -21,4 +21,13 @@ public class BurgerTest {
         burger.removeIngredient(0);
         Assert.assertEquals(ingredientsCount - 1, burger.ingredients.size());
     }
+    @Test
+    public void checkMoveIngredient() {
+        burger.addIngredient(new Ingredient(IngredientType.SAUCE, "hot sauce", 100));
+        burger.addIngredient(new Ingredient(IngredientType.FILLING, "dinosaur", 200));
+        Ingredient ingredient1 = burger.ingredients.get(1);
+        burger.moveIngredient(1, 0);
+        Ingredient ingredient2 = burger.ingredients.get(0);
+        Assert.assertEquals(ingredient1, ingredient2);
+    }
 }
